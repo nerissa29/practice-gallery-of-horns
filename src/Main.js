901cce75a4fc134fc7e3dd69b39d86cf23f3c415
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-11-30 17:49:31 +0000

    practice code gallery of horns

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HornedBeasts from './components/HornedBeasts.js';
+
+class Main extends React.Component {
+  render() {
+    return(
+      <>
+        <HornedBeasts 
+          title=''      
+        />
+      </>
+    )
+  }
+}
+
+export default Main;
---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+  render() {
+    return(
+      <>
+      </>
+    )
+  }
+}
+
+export default HornedBeasts;
