6a9747ec8b9f024b3dfb3d200ccaf59992338535
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-12-01 17:46:56 +0000

    updated HornedBeasts component

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -4,6 +4,9 @@ class HornedBeasts extends React.Component {
   render() {
     return(
       <>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageUrl} alt={this.props.title} title={this.props.title} />
+        <p>{this.props.description}</p>
       </>
     )
   }
